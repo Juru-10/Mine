20897b3ff95e72d8b6e901c409e079bc6a1588e0
JURU <jurassu10@gmail.com>
2019-03-22 18:32:33 +0200

    add the home page view

--- gram/views.py
+++ gram/views.py
@@ -3,3 +3,9 @@ from django.http  import HttpResponse,Http404
 import datetime as dt
 from .models import Image
 import pyperclip
+
+def all_images(request):
+    image=Image.objects.all()
+    date=dt.date.today()
+
+    return render(request,"all-image/today-image.html", {"date": date,"image":image})
