b7912fc7ebed8c5bcb15388fb15a19e0c423a3cf
JURU <jurassu10@gmail.com>
2019-03-22 16:32:27 +0200

    add profile path column in the users's table/model

--- gram/models.py
+++ gram/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class User(models.Model):
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length = 30)
+    full_name = models.CharField(max_length = 30)
+    username = models.CharField(max_length = 30)
     email = models.EmailField()
+    password = models.CharField(min_legth = 8)
+    prof_pic_path = models.CharField(max_length = 30)
     phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
