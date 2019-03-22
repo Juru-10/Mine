f67ccbafcd36acefcd5d1e5d94644d73ee0e0275
JURU <jurassu10@gmail.com>
2019-03-22 15:30:58 +0200

    add imports in the views.py file

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+import datetime as dt
+from .models import Image
+import pyperclip
